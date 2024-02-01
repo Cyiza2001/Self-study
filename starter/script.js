549b8ad83481119ac588b0d7f7bef8e7847d2c02
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-02-01 23:52:45 +0200

    another commit

--- starter/script.js
+++ starter/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
+//render a country using a template literal
 const renderCountry= function(data, className=''){
   const html=
   `<article class="country ${className}">
@@ -22,22 +22,43 @@ countriesContainer.insertAdjacentHTML('beforeend',html);
 countriesContainer.style.opacity=1;
 
 }
+//a function that will dislay a message on the UI whenever there is a fetching error
 const renderError = function(msg){
   countriesContainer.insertAdjacentText("beforeend",msg);
   countriesContainer.style.opacity= 1;
 }
+//getjson function is in charge of throwing errors
+const getJson = function(url,errormsg="something went wrong"){
+  return fetch(url).then((response) =>{
+    //if country is not found throw an error to display that the country is not found
+  if(!response.ok)
+    throw new Error(`${errormsg} (${response.status})`);
+  return response.json()
+ 
+})
 
+}
 const getCountryAndNeighbour= function(country){
-
-    const request = fetch(`https://restcountries.com/v2/name/${country}`).then(response => 
-      response.json()).then( data =>{
+    getJson(`https://restcountries.com/v2/name/${country}`,'country not found')
+    .then( (data) =>{
+    
         renderCountry(data[0])
+        //check if there is a neigbouring country if not caught the error
+         if(!data[0].borders)
+          {
+             throw new Error('there is no neighbour')};
+     
+        //destructure all the neighbouring countries in an arrray
         const [...neighbour]=data[0].borders;
+     
+        //push each neighbour country that has already been fetched into an array
       let neighbouringCountries = [];
-         neighbour.forEach(neigh => {  
-        neighbouringCountries.push(fetch(`https://restcountries.com/v2/alpha/${neigh}`).then( response => response.json())); 
+//loop over the array of the neighbouring countries
+         neighbour.forEach(neigh => {   
+        neighbouringCountries.push(getJson(`https://restcountries.com/v2/alpha/${neigh}`,'country not found'));
       
       });
+      //neighbouring countries are json promises i will just call a then method after 
       return neighbouringCountries;
        }).then((neighbouringCountries) => {neighbouringCountries.forEach( (response)=>{
         response.then( data2=> renderCountry(data2,'neighbour'))
@@ -45,7 +66,7 @@ const getCountryAndNeighbour= function(country){
         //
       }
      btn.addEventListener('click',function(){
-      getCountryAndNeighbour('rwanda');
+      getCountryAndNeighbour('usa');
      })
      
     
